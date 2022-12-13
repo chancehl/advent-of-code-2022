620f282200110560dabfe7783e393a7b15ef85d1
Chance Linz <chanlinz@amazon.com>
2022-12-12 16:10:02 -0800

    added day_one::part_two tests

--- src/day_one/mod.rs
+++ src/day_one/mod.rs
@@ -1,6 +1,21 @@
 #[allow(dead_code)]
 pub mod problem {
+    /// Converts "meal log" (new-line separated string of numbers) into a single u32 value that represents the sum of the meals
+    /// example: input = "1\n\2\n3" -> 6
+    pub fn calculate_sum_of_meals(input: &str) -> u32 {
+        let meals: Vec<u32> = input
+            .split("\n")
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|&v| str::parse::<u32>(&v).unwrap())
+            .collect();
+
+        meals.iter().sum()
+    }
+
     pub mod part_one {
+        use super::calculate_sum_of_meals;
+
         /// Counts calories and returns the number of calories eaten by the elf who consumed the most
         pub fn calorie_counting(input: &str) -> u32 {
             let elf_meals = input.split("\n\n").collect::<Vec<&str>>();
@@ -17,39 +32,52 @@ pub mod problem {
 
             max
         }
+    }
 
-        /// Converts "meal log" (new-line separated string of numbers) into a single u32 value that represents the sum of the meals
-        /// Example: input = "1\n\2\n3" -> 6
-        pub fn calculate_sum_of_meals(input: &str) -> u32 {
-            let meals: Vec<u32> = input
-                .split("\n")
-                .collect::<Vec<&str>>()
+    pub mod part_two {
+        use super::calculate_sum_of_meals;
+
+        /// Converts "meal log" (new-line separated string of numbers) into a single u32 value that represents the
+        /// sum of calories consumed by the top three elves
+        /// example: input = "1\n\2\n3" -> 6
+        pub fn calorie_counting(input: &str) -> u32 {
+            let elf_meals = input.split("\n\n").collect::<Vec<&str>>();
+
+            let mut calories_consumed: Vec<u32> = Vec::new();
+
+            for meal in elf_meals {
+                let calories = calculate_sum_of_meals(meal);
+
+                calories_consumed.push(calories);
+            }
+
+            calories_consumed.sort();
+
+            calories_consumed[calories_consumed.len() - 3..]
                 .iter()
-                .map(|&v| str::parse::<u32>(&v).unwrap())
-                .collect();
-
-            meals.iter().sum()
+                .sum()
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::problem::part_one::{calculate_sum_of_meals, calorie_counting};
+    use super::problem::{calculate_sum_of_meals, part_one, part_two};
 
     #[test]
     fn calculate_sum_of_meals_test() {
-        let input = "1\n2\n3";
-        let result = calculate_sum_of_meals(input);
-        let expected = 6;
-        assert_eq!(result, expected);
+        assert_eq!(calculate_sum_of_meals("1\n2\n3"), 6);
+        assert_eq!(calculate_sum_of_meals("2\n2\n2"), 6);
+        assert_eq!(calculate_sum_of_meals("5\n10\n15\n20"), 50);
     }
 
     #[test]
     fn part_one_calorie_counting_test() {
-        let input = "1\n2\n3\n\n5\n5\n5";
-        let result = calorie_counting(input);
-        let expected = 15;
-        assert_eq!(result, expected);
+        assert_eq!(part_one::calorie_counting("1\n2\n3"), 6);
+    }
+
+    #[test]
+    fn part_two_calorie_counting_test() {
+        assert_eq!(part_two::calorie_counting("5\n\n5\n\n5"), 15);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,19 @@ mod day_two;
 
 use std::fs;
 
-// use day_one::problem::calorie_counting;
+// use day_one::problem::part_one::calorie_counting;
+use day_one::problem::part_two::calorie_counting;
 // use day_two::problem::part_one::rock_paper_scissors;
 // use day_two::problem::part_two::rock_paper_scissors;
 // use day_three::problem::part_one::rucksack_reorganization;
 // use day_three::problem::part_two::rucksack_reorganization;
 // use day_four::problem::part_one::camp_cleanup;
-use day_four::problem::part_two::camp_cleanup;
+// use day_four::problem::part_two::camp_cleanup;
 
 fn main() {
-    let input = fs::read_to_string("./src/day_four/input.txt").unwrap();
+    let input = fs::read_to_string("./src/day_one/input.txt").unwrap();
 
-    let overlapping_ranges = camp_cleanup(&input);
+    let calories = calorie_counting(&input);
 
-    println!("overlapping_ranges = {:?}", overlapping_ranges);
+    println!("calories = {:?}", calories);
 }
